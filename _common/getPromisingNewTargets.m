f977321fdec94963523b476b723172aac9a46186
BaiLiping <blp_engineer@outlook.com>
2022-11-26 20:52:57 +0800

    Update getPromisingNewTargets.m

--- _common/getPromisingNewTargets.m
+++ _common/getPromisingNewTargets.m
@@ -1,37 +1,39 @@
 function [newIndexes,measurements] = getPromisingNewTargets(currentParticlesKinematic,currentParticlesExtent,currentExistences,measurements,parameters)
 
-% find ``free'' measurements by updating legacy potential targets only
-numMeasurements = size(measurements,2);
-numTargets = length(currentParticlesKinematic);
-measurementsCovariance = parameters.measurementVariance * eye(2);
-meanClutter = parameters.meanClutter;
-meanMeasurements = parameters.meanMeasurements;
-surveillanceRegion = parameters.surveillanceRegion;
-areaSize =  (surveillanceRegion(2,1)-surveillanceRegion(1,1)) * (surveillanceRegion(2,2)-surveillanceRegion(1,2));
-constantFactor = areaSize*(meanMeasurements/meanClutter);
-currentEpsilonExtrinsic = repmat([currentParticlesExtent]',[1,numMeasurements]);
-
-numParticles = arrayfun(@(x) length(x.particleWeights),currentParticlesKinematic);
-inputDA = zeros(numTargets,numMeasurements);
-
-for target = 1:numTargets
-    lengthIndex = numParticles(target);
-    tmpMean = reshape(currentParticlesKinematic(target).particlesKinematic(1:2,:),2,lengthIndex);
-    tmpCov = reshape(currentParticlesKinematic(target).particlesExtent,2,2,lengthIndex) + repmat(measurementsCovariance,[1,1,lengthIndex]);
-    for measurement = numMeasurements:-1:1
-        likelihood1{target}(:,measurement) = constantFactor * exp(getLogWeightsFast(measurements(:,measurement),tmpMean,tmpCov));
-        inputDA(target,measurement) = currentEpsilonExtrinsic(target,measurement) * currentParticlesKinematic(target).particleWeights'*likelihood1{target}(:,measurement);
+    % find ``free'' measurements by updating legacy potential targets only
+    numMeasurements = size(measurements,2);
+    numTargets = length(currentParticlesKinematic);
+    measurementsCovariance = parameters.measurementVariance * eye(2);
+    meanClutter = parameters.meanClutter;
+    meanMeasurements = parameters.meanMeasurements;
+    surveillanceRegion = parameters.surveillanceRegion;
+    areaSize =  (surveillanceRegion(2,1)-surveillanceRegion(1,1)) * (surveillanceRegion(2,2)-surveillanceRegion(1,2));
+    constantFactor = areaSize*(meanMeasurements/meanClutter);
+    currentEpsilonExtrinsic = repmat([currentParticlesExtent]',[1,numMeasurements]);
+    
+    numParticles = arrayfun(@(x) length(x.particleWeights),currentParticlesKinematic);
+    inputDA = zeros(numTargets,numMeasurements);
+    
+    for target = 1:numTargets
+        lengthIndex = numParticles(target);
+        targetdata=currentParticlesKinematic(:,:,target)
+        targetextenddata=currentParticlesExtent(:,:,target)
+        tmpMean = reshape(targetdata(1:2,:),2,lengthIndex);
+        tmpCov = reshape(targetextenddata,2,2,lengthIndex) + repmat(measurementsCovariance,[1,1,lengthIndex]);
+        for measurement = numMeasurements:-1:1
+            likelihood1{target}(:,measurement) = constantFactor * exp(getLogWeightsFast(measurements(:,measurement),tmpMean,tmpCov));
+            inputDA(target,measurement) = currentEpsilonExtrinsic(target,measurement) * currentParticlesKinematic(target).particleWeights'*likelihood1{target}(:,measurement);
+        end
     end
-end
-
-probabilitiesNew = zeros(numMeasurements,numTargets+1);
-for measurement = 1:numMeasurements
-    probabilitiesNew(measurement,:) = [inputDA(:,measurement);1]/(sum(inputDA(:,measurement))+1);
-end
-probabilitiesNew = probabilitiesNew(:,end);
-
-% find central measurements in clusters of unused measurements and reorder measurement
-[newIndexes,indexesReordered] = getCentralReordered(measurements,probabilitiesNew,measurementsCovariance,parameters);
-measurements = measurements(:,indexesReordered);
-
-end
+    
+    probabilitiesNew = zeros(numMeasurements,numTargets+1);
+    for measurement = 1:numMeasurements
+        probabilitiesNew(measurement,:) = [inputDA(:,measurement);1]/(sum(inputDA(:,measurement))+1);
+    end
+    probabilitiesNew = probabilitiesNew(:,end);
+    
+    % find central measurements in clusters of unused measurements and reorder measurement
+    [newIndexes,indexesReordered] = getCentralReordered(measurements,probabilitiesNew,measurementsCovariance,parameters);
+    measurements = measurements(:,indexesReordered);
+    
+    end
